6f9f52f963ea72a96387c5a25938397e0f3432af
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2025-02-12 15:00:08 +0500

    1st-class

---
+++ 1st-class/index.py
@@ -0,0 +1,20 @@
+# 1st program print ('Hello world!')
+print("Hello World!")
+
+# variables
+name = ('Shahid')          # string
+
+age = 25                   # integer
+
+is_work = True             # boolean
+
+value = 44.45444           # float
+
+print(name,age,is_work,value)
+
+
+# Data types in python
+print (type(name))          # string
+print (type(age))           # integer
+print (type(is_work))       # boolean
+print (type(value))         # float
